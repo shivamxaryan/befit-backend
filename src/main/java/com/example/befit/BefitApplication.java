a37ac035356feca61009b762f22ee3d46a64549c
Shivam <shivamaryan64@gmail.com>
2026-03-11 22:22:05 +0530

    feat: initial setup

---
+++ src/main/java/com/example/befit/BefitApplication.java
@@ -0,0 +1,13 @@
+package com.example.befit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BefitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BefitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/befit/BefitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.befit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BefitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
